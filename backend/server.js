67a5bce66cd844f5a67616f33aae7e8c74a9c198
Hammad Javed <82704549+Hammadnmt@users.noreply.github.com>
2025-04-27 15:24:20 +0500

    / removed

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const errorHandler = require("./middleware/errorMiddleware");
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "https://buspro-bgjy.vercel.app/",
+  origin: "https://buspro-bgjy.vercel.app",
   exposedHeaders: ["Set-Cookie"],
   credentials: true,
 };
